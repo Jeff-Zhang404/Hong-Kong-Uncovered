faa42c512036566b3f08e522a3321df4c82ac544
Jeff-Zhang404 <jeff877300028zxt@gmail.com>
2026-09-22 15:12:27 -0700

    Fix GitHub Pages deployment path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/p12/',
+  base: '/Hong-Kong-Uncovered/',
   build: {
     outDir: 'docs'
   }
